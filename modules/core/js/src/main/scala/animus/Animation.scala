a9ffc6b3c8619f02f1503d090702eea400bc3a13
Scala Steward <scala_steward@virtuslab.com>
2024-07-29 17:23:54 +0000

    Reformat with scalafmt 3.8.3
    Executed command: scalafmt --non-interactive

--- modules/core/js/src/main/scala/animus/Animation.scala
+++ modules/core/js/src/main/scala/animus/Animation.scala
@@ -5,18 +5,18 @@ import com.raquo.airstream.state.Var
 
 import scala.scalajs.js.timers.setTimeout
 
-sealed trait Animation { self =>
-  import Animation._
+sealed trait Animation:
+  self =>
+  import Animation.*
 
-  def to(double: => Double): Animation = {
+  def to(double: => Double): Animation =
     lazy val double0 = double
     Sequence(self, Value(() => double0))
-  }
 
   def wait(ms: Int): Animation = Hold(ms, self)
 
   def flattened: Vector[(() => Double, Double)] =
-    self match {
+    self match
       case Value(value) => Vector((value, 300))
       case Hold(delay, animation) =>
         val flatAnim                            = animation.flattened
@@ -25,9 +25,8 @@ sealed trait Animation { self =>
         res
       case Sequence(lhs, rhs) =>
         lhs.flattened ++ rhs.flattened
-    }
 
-  def signal: Signal[Double] = {
+  def signal: Signal[Double] =
     val flat     = flattened
     val variable = Var(flat.head._1())
 
@@ -41,19 +40,14 @@ sealed trait Animation { self =>
     go(flat)
 
     variable.signal
-  }
 
   def run: Signal[Double] = signal.spring
 
-}
-
-object Animation {
-  def from(double: => Double): Animation = {
+object Animation:
+  def from(double: => Double): Animation =
     lazy val double0 = double
     Value(() => double0)
-  }
 
   private case class Value(value: () => Double)               extends Animation
   private case class Hold(delay: Int, animation: Animation)   extends Animation
   private case class Sequence(lhs: Animation, rhs: Animation) extends Animation
-}
